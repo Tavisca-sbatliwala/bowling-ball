9329f637c30f6525c0ace5ca4cc129dcdb0fd6c0
saif2752 <sbatliwala@tavisca.com>
2014-07-15 18:15:46 +0530

    Bowling problem with 6 test cases

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,112 @@ namespace Bowling
 {
     public class Game
     {
-        public void Roll(int pins)
-        {
-            throw new NotImplementedException();
-        }
+        private List<Int32> myRolls = new List<Int32>();
+        private int rollCurrent;
+        private int finalScore;
+        
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            finalScore = 0;
+            rollCurrent = 0;
+            ComputeAllRollScore();
+            return finalScore;
         }
 
+        private void ComputeAllRollScore()
+        {
+
+            if(myRolls.Count != 20) 
+            {
+                if (myRolls.Count == 19)
+                {
+                    for (int frames = 0; frames < 10; frames++)
+                    {
+                        if (myRolls[rollCurrent] == 10)
+                            ScoreStrike();
+                        else if (SumUpRollsFromStart(2) == 10)
+                            ScoreSpare();
+                        else
+                            ScoreNormal();
+                    }
+                }
+
+                if (myRolls.Distinct().Count() == 1)  //this is for all strike
+                {
+
+                    for (int frames = 0; frames < 10; frames++)
+                    {
+                        if (myRolls[rollCurrent] == 10)
+                            ScoreStrike();
+                        else if (SumUpRollsFromStart(2) == 10)
+                            ScoreSpare();
+                        else
+                            ScoreNormal();
+                    }
+
+                }
+                else 
+                {
+                    for (int frames = 0; frames < (myRolls.Count / 2); frames++)
+                    {
+                        if (myRolls[rollCurrent] == 10)
+                            ScoreStrike();
+                        else if (SumUpRollsFromStart(2) == 10)
+                            ScoreSpare();
+                        else
+                            ScoreNormal();
+                    }
+                }
+
+            }
+           
+            else
+            {
+                for (int frames = 0; frames < 10; frames++)
+                {
+                    if (myRolls[rollCurrent] == 10)
+                        ScoreStrike();
+                    else if (SumUpRollsFromStart(2) == 10)
+                        ScoreSpare();
+                    else
+                        ScoreNormal();
+                }
+            }
+        }
+
+        private int SumUpRollsFromStart(int noOfRolls)
+        {
+             int sum = 0;
+             for (int i = 0; i < noOfRolls; i++)
+             {
+                 sum = sum + myRolls[rollCurrent + i];
+             }
+            return sum;
+        }
+
+        private void ScoreNormal()
+        {
+            finalScore =finalScore+ SumUpRollsFromStart(2);
+            rollCurrent =rollCurrent+ 2;
+        }
+
+        private void ScoreSpare()
+        {
+            finalScore =finalScore + SumUpRollsFromStart(3);
+            rollCurrent =rollCurrent + 2;
+        }
+
+        private void ScoreStrike()
+        {
+            finalScore =finalScore + SumUpRollsFromStart(3);
+            rollCurrent=rollCurrent+1;
+        }
+
+        public void Roll(int pins)
+        {
+            myRolls.Add(pins);
+        }
     }
 }
+
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bowling;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BowlingFixtures
@@ -6,9 +7,84 @@ namespace BowlingFixtures
     [TestClass]
     public class GameFixture
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Game myGame=new Game();
+
+        private void RollwithSamePin(int numberOfChances, int pins)
         {
+            for (int i =0; i < numberOfChances; i++)
+            {
+                myGame.Roll(pins);
+            }
         }
+
+        [TestMethod]
+        public void AllGutterBalls() //all gutterball then score is zero
+        {
+            RollwithSamePin(20, 0);
+            int score = myGame.GetScore();
+            Assert.AreEqual(0, score);
+        }
+
+        [TestMethod]
+        public void AllBallDropOne()
+        {
+            RollwithSamePin(20, 1);
+            int score = myGame.GetScore();
+            Assert.AreEqual(20, score);
+        }
+
+        [TestMethod]
+        public void AllStrike()
+        {
+            RollwithSamePin(12, 10);        
+            int score = myGame.GetScore();
+            Assert.AreEqual(300, score);
+        }
+
+        [TestMethod]
+        public void FirstFrameSpareThenAllOnes()
+        {
+            RollwithSamePin(2, 5);
+            RollwithSamePin(18, 1);
+            int score = myGame.GetScore();
+            Assert.AreEqual(29, score);
+        }
+
+        [TestMethod]
+        public void AllFrameSpare()
+        {
+            RollwithSamePin(21, 5);
+            int score = myGame.GetScore();
+            Assert.AreEqual(150, score);
+        }
+
+        //testcase random
+        [TestMethod]
+        public void TestRandom1()
+        {
+            myGame.Roll(2);
+            myGame.Roll(3);
+            myGame.Roll(8);
+            myGame.Roll(1);
+            myGame.Roll(7);
+            myGame.Roll(2);
+            myGame.Roll(1);
+            myGame.Roll(2);
+            myGame.Roll(4);
+            myGame.Roll(5);
+            myGame.Roll(7);
+            myGame.Roll(1);
+            myGame.Roll(3);
+            myGame.Roll(4);
+            myGame.Roll(7);
+            myGame.Roll(2);
+            myGame.Roll(9);
+            myGame.Roll(0);
+            int score = myGame.GetScore();
+            Assert.AreEqual(68, score);
+
+        }
+
+
     }
 }
